c0245efc72080b0f0990525e400de60e6985af03
Nighthawk <git@nighthawk.cc>
2020-07-29 00:32:10 +0800

    git submodule init

---
+++ workflows.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Env
+export WORKDIR=$(pwd)
+export SOURCE=raspberrypi-kernel
+export OUTPUT=temp
+export OUTPUT_REPO=kernel
+export ARCH=arm64
+export TARGET=shrink_defconfig
+
+# Prepare
+## mkdir
+cd ${WORKDIR}
+mkdir ${OUTPUT}
+mkdir ${OUTPUT_REPO}
+
+## clean
+cd ${WORKDIR}/${SOURCE}
+make mrproper
+cd ${WORKDIR}/${OUTPUT_REPO}
+git rm -r .
+cd ${WORKDIR}/${OUTPUT}
+rm -rf *
+
+# Compile
+cd ${WORKDIR}/${SOURCE}
+## kernel
+make ${TARGET}
+make -j96
+
+## kernel modules
+make INSTALL_MOD_PATH=${WORKDIR}/${OUTPUT}/ modules_install
+
+# Collect
+## kernel
+cp ${WORKDIR}/${SOURCE}/arch/${ARCH}/boot/Image ${WORKDIR}/${OUTPUT}/
+
+## device tree binary
+cp ${WORKDIR}/${SOURCE}/arch/${ARCH}/boot/dts/broadcom/*.dtb ${WORKDIR}/${OUTPUT}/
+mkdir ${WORKDIR}/${OUTPUT}/overlays
+cp ${WORKDIR}/${SOURCE}/arch/${ARCH}/boot/dts/overlays/*.dtb* ${WORKDIR}/${OUTPUT}/overlays/
+
+# Finish
+cd ${WORKDIR}
+## size
+SIZE=$(du -d 0 ${OUTPUT} | awk '{print $1}')
+## target_defconfig
+cp ${WORKDIR}/${SOURCE}/arch/${ARCH}/configs/${TARGET} ${WORKDIR}/${OUTPUT}
+## mv
+cp -rf ${WORKDIR}/${OUTPUT}/* ${WORKDIR}/${OUTPUT_REPO}/
+
+# Git
+cd ${WORKDIR}/${OUTPUT_REPO}
+git add .
+git commit -m "${SIZE}"
